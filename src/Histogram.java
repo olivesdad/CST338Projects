8d74a8017b4b6da9d4d5427b23e5846a14664b2e
andy <ashiraki@anduril.com>
2021-10-30 11:17:20 -0700

    added instruction files

---
+++ src/Histogram.java
@@ -0,0 +1,26 @@
+public class Histogram {
+
+    public static void main(String[] args){
+        //This will be the driver for our program. It will call the following methods.
+    }
+
+    public static String getFileName(){
+        //This method will read the filename from the user
+        return "not done";
+    }
+
+    public static void read(char[] letter, int[] letterCount, String filename){
+        //This method will read the letters from the filename provided in the previous method.
+        //Use this to populate the int[] letterCount
+    }
+
+    public static void sort(char[] letter, int[] letterCount){
+        //This method will sort the letters
+    }
+
+    public static void display(char[] letter, int[] count){
+        //This method will display the histogram
+    }
+
+
+}
