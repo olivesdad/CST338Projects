7bc937f60c75124c5a61a16809c68c85653d8ecd
andy <ashiraki@anduril.com>
2021-10-30 12:37:33 -0700

    progress

--- src/Histogram.java
+++ src/Histogram.java
@@ -1,17 +1,50 @@
+/*
+* Histogram.java
+* Andrew Shiraki
+* 2021-10-30
+* This is the Histogram class it will read file input from the file name given by the user through cli
+* and output a formatted histogram
+*/
+import java.io.FileNotFoundException;
+import java.util.*;
+import java.io.File;
+import java.util.logging.FileHandler;
+
 public class Histogram {
 
     public static void main(String[] args){
-        //This will be the driver for our program. It will call the following methods.
+       //variables
+        char[]letters = new char [200];
+        int[] letterCount = new int[11];
+
+        //-----<>..<>------//
+       read(letters,letterCount,getFileName());
     }
 
+    //Method to read filename from cli
     public static String getFileName(){
-        //This method will read the filename from the user
-        return "not done";
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter file name: ");
+        return scanner.nextLine();
     }
 
     public static void read(char[] letter, int[] letterCount, String filename){
         //This method will read the letters from the filename provided in the previous method.
         //Use this to populate the int[] letterCount
+        //make file object
+        File file = new File(filename);
+        int count=0;
+        Scanner scanner=null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+        //populate letter array
+        while (scanner.hasNext()){
+        letter[count++] = scanner.nextLine().charAt(0);
+        System.out.println(letter[count-1]);
+        }
     }
 
     public static void sort(char[] letter, int[] letterCount){
