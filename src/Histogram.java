e98189a8d1217cfb2553aef164ab33300bfdb68f
andy <ashiraki@anduril.com>
2021-10-30 23:31:37 -0700

    fix out of bounds checking when no 0 count letters

--- src/Histogram.java
+++ src/Histogram.java
@@ -46,7 +46,7 @@ public class Histogram {
             assert scanner != null;
             letterCount[(int) scanner.nextLine().charAt(0) - 65]++;
         } while (scanner.hasNext());
-        
+
     }
 
     //pass the letter array and the count array, order them both from lowest to highest
@@ -82,26 +82,26 @@ public class Histogram {
     }
 
     public static void display(char[] letter, int[] count) {
-        //print section 1 all occurrences in descending order
+        //~~~~~~~//Print the First Style thing~~~~~~~~~~~//
         System.out.println("Char Occurrence:");
-        //find lowest occurrence
+        //find lowest occurrence so that we can count backwards skipping 0s
         int marker = 0;
         while (marker < 10 && count[marker+1] != 0 ){
             marker++;
         }
-        //print from marker down
+        //print from marker and count down
         for (int i = marker; i >= 0; i--) {
                 System.out.println(letter[i] + " " + count[i]);
         }
-        //print section 2
-        System.out.println("\n" + "================");
+        //~~~~~~~//Print the Second Style thing~~~~~~~~~~~//
+        System.out.println("================");
         //loop through the count array
         marker = 0;
         String letters = "";
         int letterCount = count [0];//start here
         while (marker < 11 && count[marker] !=0){ //<~iterate through the entire char array of A-K and add any letters that occur to the string
             letterCount = count[marker];
-            while (letterCount == count[marker] && marker < 11){
+            while  (marker < 11 && letterCount == count[marker] ){//put marker limit check first to not test out of bounds
                 letters = letters + letter[marker++]; //at letter to string
             }
             if(letterCount > 0){ //if there was more than 0 of this letter print the number of occurrences. We need to sort after all strings are added.
@@ -110,12 +110,12 @@ public class Histogram {
         }
         System.out.println("----------------");
 
-        //print the last thing
-        String theEnd = "";
+        //~~~~~//just print the sorted letter array reversed//~~~~~~~//
+        StringBuilder theEnd = new StringBuilder();//intellij says I should use stringbuilder
         for (int i = letter.length -1 ; i >=0; i--){
-            theEnd = theEnd + letter[i];
+            theEnd.append(letter[i]);
         }
-        System.out.printf("%16s%n", theEnd);
+        System.out.printf("%16s%n", theEnd.toString());
     }
     //takes the letter count string and the letters string, formats them and returns string in the required format
     public static String outputString(int num, String letters){
