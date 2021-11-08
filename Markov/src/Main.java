ea0347fa22633b5872efd181b6bbbb51bb9533dc
andy <ashiraki@anduril.com>
2021-11-07 22:02:27 -0800

    comment up dis mutha

--- Markov/src/Main.java
+++ Markov/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main (String[] args) {
         Markov markov = new Markov();
 
-        markov.addFromFile("hamlet.txt");
+        markov.addFromFile("phrases.txt");
         System.out.println(markov);
 
         for (int i = 0; i < 10; i++) {
