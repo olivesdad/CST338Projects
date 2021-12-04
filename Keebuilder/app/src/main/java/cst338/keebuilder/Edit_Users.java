4150b3e328e3f2075c2f6baebe9776b78c23aa84
andy <ashiraki@anduril.com>
2021-12-04 11:39:29 -0800

    start work on admin options

---
+++ Keebuilder/app/src/main/java/cst338/keebuilder/Edit_Users.java
@@ -0,0 +1,14 @@
+package cst338.keebuilder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Edit_Users extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_users);
+    }
+}
